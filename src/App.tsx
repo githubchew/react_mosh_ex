95512d6c0e59452502ff25ef101084bd4d8fcd1f
lmde-git-hoychew <iammusiccchu@gmail.com>
2024-07-25 15:29:50 +0800

    handling submission

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import "bootstrap/dist/css/bootstrap.css";
 import Button from "./components/Buttons";
-
+import { useState } from "react";
+import { BsFillCalendarFill } from "react-icons/bs";
+import Like from "./components/Like";
+import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 function App() {
   return (
     <div>
-      <Button color="secondary" onClick={() => console.log("clicked")}>
-        My button
-      </Button>
-      <Button onClick={() => console.log("new btn")}>new button</Button>
+      <Form />
     </div>
   );
 }
-
 export default App;
