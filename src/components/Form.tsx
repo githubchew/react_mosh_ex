7a07fd91b1cac02eaab68d76cb0a7394a2ae660d
lmde-git-hoychew <iammusiccchu@gmail.com>
2024-07-26 11:28:51 +0800

    delete everything for new project

--- src/components/Form.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FormEvent } from "react";
-import { useForm } from "react-hook-form";
-
-const Form = () => {
-  const { register } = useForm();
-  console.log(register("name"));
-};
-export default Form;
